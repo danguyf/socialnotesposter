c0a4441ea4f03388f4ec165729c10ca4fae21cb5
Dan Guy <maps@p0mi.com>
2026-01-18 13:56:59 -0500

    Updated gradle config to remove deprecated bits.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.ksp)
 }
 
@@ -28,8 +27,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
+
+    buildFeatures {
+        resValues = true
     }
 }
 
